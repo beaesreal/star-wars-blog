cf10241e2fc962f9ef3636b20daf849d707e815e
beaesreal <beaesreal@gmail.com>
2022-10-26 17:08:28 +0000

    Exercise WIP. Connected with API

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,8 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			May the force be with you to complete the Bootcamp 💻 Made with ♥️ by Beatriz Real @ 4Geeks Academy
 		</p>
 	</footer>
 );
